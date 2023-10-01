c3e081ab903ee30c117b2d810925fdffc62a00eb
damien <damien.clementdhuart@gmail.com>
2023-10-01 19:16:07 +0200

    add jdbc state repository

--- runtime/src/main/java/io/quarkiverse/togglz/runtime/StateRepositoryRecorder.java
+++ runtime/src/main/java/io/quarkiverse/togglz/runtime/StateRepositoryRecorder.java
@@ -2,7 +2,10 @@ package io.quarkiverse.togglz.runtime;
 
 import java.util.function.Function;
 
+import javax.sql.DataSource;
+
 import org.togglz.core.repository.StateRepository;
+import org.togglz.core.repository.jdbc.JDBCStateRepository;
 import org.togglz.core.repository.mem.InMemoryStateRepository;
 
 import io.quarkus.arc.SyntheticCreationalContext;
@@ -10,7 +13,16 @@ import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
 public class StateRepositoryRecorder {
-    public Function<SyntheticCreationalContext<StateRepository>, StateRepository> createStateRepository() {
+    public Function<SyntheticCreationalContext<StateRepository>, StateRepository> createInMemoryStateRepository() {
         return (context) -> new InMemoryStateRepository();
+    }
+
+    public Function<SyntheticCreationalContext<StateRepository>, StateRepository> createJDBCStateRepository() {
+        return (context) -> {
+            final DataSource dataSource = context.getInjectedReference(DataSource.class);
+            return new JDBCStateRepository.Builder(dataSource)
+                    .usePostgresTextColumns(true)
+                    .build();
+        };
     }
 }
